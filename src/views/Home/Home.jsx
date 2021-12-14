eaa38ede7bc6565b93728f999555e012d7093396
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-12-13 17:34:21 -0800

    whoops overcomplicated everything & rolling back

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,21 @@
-import LikeList from '../../components/LikeList/LikeList'
-import { LikeProvider } from '../../context/UserContext'
+import { useEffect, useState } from 'react'
+import Profile from '../../components/Profile/Profile'
+import { useUser } from '../../context/UserContext'
 
 const Home = () => {
-  ;<LikeProvider>
-    <LikeList />
-  </LikeProvider>
+  const { user } = useUser()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (user.name) {
+      setLoading(false)
+    } else {
+      setLoading(true)
+    }
+  }, [user])
+
+  if (loading) return <h1>Loading...</h1>
+  return <Profile user={user} />
 }
 
 export default Home
